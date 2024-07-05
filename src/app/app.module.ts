03c6b17154a4e74fec84bccdef106a014d6f35aa
Debora Campelo <deboracampelo1978@gmail.com>
2024-07-05 15:29:44 -0300

    Criação do Formulário de Cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './material.module'; // Importe o MaterialModule aqui
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ClientFormComponent } from './client-form/client-form.component';
 import { ContratoFormComponent } from './contrato-form/contrato-form.component';
-import { ContratoListGridComponent } from './contrato-list-grid/contrato-list-grid.component'
+import { ContratoListGridComponent } from './contrato-list-grid/contrato-list-grid.component';
 
 @NgModule({
   declarations: [
@@ -21,13 +20,12 @@ import { ContratoListGridComponent } from './contrato-list-grid/contrato-list-gr
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     FormsModule,
-    MaterialModule,
+    MaterialModule, // Importe o MaterialModule aqui
     FlexLayoutModule
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
